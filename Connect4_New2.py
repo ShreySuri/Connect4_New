30a046b3567558ddbe1fddef4f277eaf43a25060
ShreySuri <shreysuri5@gmail.com>
2022-09-05 18:38:27 -0500

    Update Connect4_New2.py

--- Connect4_New2.py
+++ Connect4_New2.py
@@ -11,11 +11,27 @@ def board(list_42):
         print(string)
 
 
-list_1 = []
+def game_format(list_42, input_1, symbol):
+    list_42.reverse()
+    input_1 = 8 - input_1
+    list_val = list_42[input_1]
+    current_pos = input_1
+    count = 0
+    while list_val != " " or count > 6:
+        current_pos = current_pos + 7
+        list_val = list_42[current_pos]
+        count = count + 1
+
+    if list_val == " ":
+        list_42[current_pos] = symbol
+        return list_42
+    else:
+        return False
+        
+list_42 = []
 for i in range (0, 42):
-    if i < 10:
-        i = str("0%s" % i)
-    list_1.append(i)
+    list_42.append(" ")
 
-board(list_1)
+list_42 = game_format(list_42, 5, "x")
 
+board(list_42)
